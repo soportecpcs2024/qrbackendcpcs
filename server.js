32f0ae70f838a1feb38d77a8faefb8bf5d7766a2
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2024-09-12 11:09:02 -0500

    cors

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
-app.use(cors());
+// Configurar CORS para permitir solicitudes desde http://localhost:5173
+const corsOptions = {
+  origin: 'http://localhost:5173', // URL de tu frontend
+  methods: 'GET', // Métodos HTTP permitidos (puedes agregar más si es necesario)
+  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+};
+
+app.use(cors(corsOptions)); // Usar las opciones de CORS configuradas
 app.use(express.json());
 
 const messages = {
   '1': 'Hola',
   '2': 'Qué más',
   '3': 'Chao',
-  '4': 'Nos vemos'
+  '4': 'Nos vemos',
 };
 
 app.get('/message/:id', (req, res) => {
