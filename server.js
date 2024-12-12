0b1d447cfd73ebe4760b91dc99aeab6268ae7caf
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2024-12-12 08:55:11 -0500

    actualización vista

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/message/:id', async (req, res) => {
               h2 { font-size: 24px; margin-bottom: 10px; }
               p { font-size: 14px; margin: 3px 0; }
               img { max-width: 80%; height: auto; margin-top: 10px; }
-              h4 { font-size: 14px; margin: 3px 0; }
+              h4 { font-size: 16px; margin-top: 10px 0; }
           </style>
       </head>
       <body>
